73f20ddb9acb4e840919d2cea9f2a6bae1a69c78
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-04-25 09:05:41 -0500

    cleanup

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Uses msbuild to compile the release version of the plugin. A distributable zip will be placed in your working dir.
 VERSION="2.0.0-BETA"
 workingDir=$(pwd)
-msbuild Tebex-TorchAPI.sln -p:Configuration=Release
+msbuild ./Tebex-TorchAPI/Tebex-TorchAPI.sln -p:Configuration=Release
 cd ./Tebex-TorchAPI/bin/x64/Release
-mv Tebex-TorchAPI.dll Tebex-TorchAPI-${VERSION}.dll
+mv TebexTorchAPI.dll Tebex-TorchAPI-${VERSION}.dll
 zip "Tebex-TorchAPI-${VERSION}.zip" Tebex-TorchAPI-${VERSION}.dll
 mv "Tebex-TorchAPI-${VERSION}.zip" "${workingDir}"
 cd $workingDir
