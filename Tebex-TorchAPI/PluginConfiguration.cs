07f6723fc82be76bdb0babb153043c16bd9c355b
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-04-29 14:54:44 -0500

    Update PluginConfiguration.cs

--- Tebex-TorchAPI/PluginConfiguration.cs
+++ Tebex-TorchAPI/PluginConfiguration.cs
@@ -1,6 +1,13 @@
 ﻿using System.ComponentModel;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using Sandbox.Game.Screens.Helpers;
+using TebexSpaceEngineersPlugin.Commands;
 using Torch;
 using Torch.Views;
+using VRage;
+using VRage.Game;
 using VRage.Plugins;
 
 namespace TebexSpaceEngineersPlugin
@@ -37,7 +44,58 @@ namespace TebexSpaceEngineersPlugin
         private bool _disableRedeemCommand = false;
         [Display(Name = "Disable Redeem Command", Description = "Disable to prevent players from using /tebex:redeem")]
         [Category("Tebex Config")]
-        public bool DisableRedeemCommand {get => _disableRedeemCommand; set => SetValue(ref _disableRedeemCommand, value);
+        public bool DisableRedeemCommand
+        {
+            get => _disableRedeemCommand;
+            set => SetValue(ref _disableRedeemCommand, value);
+        }
+
+        private MyObjectBuilder_Toolbar _vanillaBacking;
+
+        [XmlIgnore]
+        private MyObjectBuilder_Toolbar VanillaDefaultToolbar => _vanillaBacking ?? (_vanillaBacking = new MyToolbar(MyToolbarType.Character, 9, 9).GetObjectBuilder());
+
+        private MyObjectBuilder_Toolbar _defaultToolbar;
+        
+        public bool ShouldSerializeDefaultToolbar()
+        {
+            return _defaultToolbar != null;
+        }
+
+        /// <summary>
+        /// Allows us to use Keen's serializer without losing previously stored config data
+        /// </summary>
+        public class ToolbarWrapper : IXmlSerializable
+        {
+            public MyObjectBuilder_Toolbar Data { get; set; }
+
+            public XmlSchema GetSchema()
+            {
+                return null;
+            }
+
+            public void ReadXml(XmlReader reader)
+            {
+                var ser = MyXmlSerializerManager.GetSerializer(typeof(MyObjectBuilder_Toolbar));
+                var o = ser.Deserialize(reader);
+                Data = (MyObjectBuilder_Toolbar)o;
+            }
+
+            public void WriteXml(XmlWriter writer)
+            {
+                var ser = MyXmlSerializerManager.GetSerializer(typeof(MyObjectBuilder_Toolbar));
+                ser.Serialize(writer, Data);
+            }
+
+            public static implicit operator MyObjectBuilder_Toolbar(ToolbarWrapper o)
+            {
+                return o.Data;
+            }
+
+            public static implicit operator ToolbarWrapper(MyObjectBuilder_Toolbar o)
+            {
+                return new ToolbarWrapper(){Data = o};
+            }
         }
     }
 }
